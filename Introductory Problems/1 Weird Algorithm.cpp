a54880afaa76764fb0c1202d5e67fdc3321cfe61
naresh2002 <naresh6436@gmail.com>
2021-04-15 15:34:48 +0530

    Added 1 Weird Algorithm

--- Introductory Problems/1 Weird Algorithm.cpp
+++ Introductory Problems/1 Weird Algorithm.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+#define PB push_back
+#define int long long
+#define endl '\n'
+const int MOD = (int)1e9 + 7;
+const double PI = acos(-1.0);
+using namespace std;
 
+
+int32_t main () {
+  int n;
+	cin >> n;
+	while (n > 1) {
+		cout << n << " ";
+		if (n % 2) {
+			n *= 3;
+			++n;
+		}
+		else{
+			n /= 2;
+		}
+	}
+	cout << 1;
+  
+  return 0;
+}
